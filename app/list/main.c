c1fe698e396a3892af35105213fbc42f207877f5
zhouyang209117 <zhouyang209117@sina.com>
2020-07-12 18:38:41 +0800

    支持汉语.

--- app/list/main.c
+++ app/list/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
 #include "list/list.h"
 typedef struct Student_ {
     int num;
-    char* name;
+    wchar_t* name;
 }Student;
 
 int main() {
+    setlocale(LC_ALL, "zh_CN.UTF-8");
     SqList* list = CreateSqList(10, sizeof(Student));
     Student stu;
     stu.num = 2;
-    stu.name = "zhang3";
+    stu.name = L"张三";
     list->Add(list, (void*)(&stu), sizeof(Student));
     printf("%d\n", list->length);
     printf("%d\n", list->total);
     printf("%d\n", ((Student*)(list->ele))[0].num);
-    printf("%s\n", ((Student*)(list->ele))[0].name);
+    printf("%ls\n", ((Student*)(list->ele))[0].name);
 }
