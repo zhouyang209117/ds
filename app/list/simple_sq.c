0524e9819d5a25887f4e276c72005ab9f496d080
zhouyang209117 <zhouyang209117@sina.com>
2020-07-24 08:13:55 +0800

    顺序表简单demo

---
+++ app/list/simple_sq.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <code/code.h>
+
+typedef struct SqList_ {
+    int* ele;
+    int total;
+    int length;
+}SqList;
+
+SqList* CreateSqList(int total) {
+    SqList* list = (SqList*)malloc(sizeof(SqList));
+    if (list == NULL) {
+        return NULL;
+    }
+    list->length = 0;
+    list->total = total;
+    list->ele = (int*)malloc(sizeof(int) * total);
+    return list;
+}
+
+int Add(SqList* list, int index, int data) {
+    if (list->length == list->total) {
+        return EAGAIN;
+    }
+    if (index < 0 || index > list->length) {
+        return EAGAIN;
+    }
+    int i = list->length;
+    for (; i > index; i--) {
+        *(list->ele + i) = *(list->ele + i - 1);
+    }
+    *(list->ele + i) = data;
+    list->length += 1;
+    return 0;
+}
+
+int main() {
+    SqList* list = CreateSqList(100);
+    Add(list, 0, 11);
+    Add(list, 0, 12);
+    Add(list, 0, 13);
+    Add(list, 0, 14);
+    Add(list, 0, 15);
+    Add(list, 1, 16);
+    for (int i = 0; i < list->length; i++) {
+        printf("%d\n", *(list->ele + i));
+    }
+    return 0;
+}
