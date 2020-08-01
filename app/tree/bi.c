d2b7121b30de07905bfaf626648ae4a8c626f959
zhouyang209117 <zhouyang209117@sina.com>
2020-08-01 17:40:46 +0800

    二叉树,初步.

---
+++ app/tree/bi.c
@@ -0,0 +1,62 @@
+#include <list/sq.h>
+#include <tree/bi.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <locale.h>
+
+typedef struct Student_ {
+    int num;
+    wchar_t* name;
+}Student;
+
+Student* CreateStudent(int num, wchar_t* name) {
+    Student* stu = (Student*)malloc(sizeof(Student));
+    stu->num = num;
+    stu->name = name;
+    return stu;
+}
+
+bool empty(void* data) {
+    return ((Student*)data)->num == -1;
+}
+
+void printStu(void* stu) {
+    Student* tmp = (Student*)stu;
+    if (tmp == NULL) {
+        printf("null pointer error\n");
+        return;
+    }
+    printf("num=%d,name=%ls\n", tmp->num, tmp->name);
+}
+
+void simpleTra(BiNode* t) {
+    if (t != NULL) {
+        printStu(t->data);
+        simpleTra(t->l);
+        simpleTra(t->r);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "zh_CN.UTF-8");
+    SqList* list = CreateSqList(sizeof(Student));
+    list->Add(list, list->length, CreateStudent(0, L"A"));
+    list->Add(list, list->length, CreateStudent(0, L"B"));
+    list->Add(list, list->length, CreateStudent(0, L"C"));
+    list->Add(list, list->length, CreateStudent(-1, L"#"));
+    list->Add(list, list->length, CreateStudent(-1, L"#"));
+    list->Add(list, list->length, CreateStudent(0, L"D"));
+    list->Add(list, list->length, CreateStudent(0, L"E"));
+    list->Add(list, list->length, CreateStudent(-1, L"#"));
+    list->Add(list, list->length, CreateStudent(0, L"G"));
+    list->Add(list, list->length, CreateStudent(-1, L"#"));
+    list->Add(list, list->length, CreateStudent(-1, L"#"));
+    list->Add(list, list->length, CreateStudent(0, L"F"));
+    list->Add(list, list->length, CreateStudent(-1, L"#"));
+    list->Add(list, list->length, CreateStudent(-1, L"#"));
+    list->Add(list, list->length, CreateStudent(-1, L"#"));
+    BiTree* tree = CreateBiTree(list, empty);
+    simpleTra(tree->head);
+    return 0;
+}
