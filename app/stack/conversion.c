84c5d17d6b570c6059c5ed3d1cb42a2be6a311af
zhouyang209117 <zhouyang209117@sina.com>
2020-08-14 07:38:47 +0800

    conversion

---
+++ app/stack/conversion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stack/link.h>
+
+void con(int N, int d) {
+    LStack* stack = CreateLStack(sizeof(int));
+    do {
+        int a = N % d;
+        stack->Push(stack, &a);
+        N /= d;
+    } while(N != 0);
+    while (!stack->Empty(stack)) {
+        printf("%d", *((int*)stack->Pop(stack)));
+    }
+}
+
+int main() {
+    con(211987, 8);
+    return 0;
+}
